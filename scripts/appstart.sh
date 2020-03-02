7a4d5e44cf72db422e4a2466a7a1d69074b25f9d
aman <sandhuharjodh2561@seasiainfotech.com>
2020-03-02 13:02:14 +0530

    added commit files

---
+++ scripts/appstart.sh
@@ -0,0 +1,2 @@
+sudo a2enmod rewrite
+sudo sytemctl restart apache2
--- scripts/beforeinstall.sh
+++ scripts/beforeinstall.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
-sudo apt-get update
-sudo apt-get install ruby
-sudo apt-get install wget
-cd /home/ubuntu
-wget https://hyla-root.s3.us-east-1.amazonaws.com/latest/install
-chmod +x ./install
-sudo ./install auto
-sudo service codedeploy-agent start
-sudo apt-get install nginx -y
-sudo systemctl start nginx
+sudo apt-get install apache2 -y
+sudo systemctl start apache2
+
+sudo rm -r -f /var/www/html/*
 
 
  
